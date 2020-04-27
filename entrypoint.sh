ce3079836ab9aaeca8930ca9ea0b8da7cb151e17
Anatoliy Bazko <abazko@redhat.com>
2020-04-27 14:01:29 +0300

    Improve script
    Signed-off-by: Anatoliy Bazko <abazko@redhat.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,16 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
+echo "[INFO] Docker registry host name: "${DOCKER_REGISTRY}
 for image in $(echo $DOCKER_IMAGES | tr ',' '\n')
 do
   if [[ ! $image =~ (docker.io|quay.io).* ]]; then
-    echo "[INFO] "$image": prefix not found. Added default: docker.io"
+    echo "[INFO] "$image" prefix not found. Added default: docker.io"
     image=docker.io/$image
   fi
-  name=$(echo $image | cut -d '/' -f3)
+  name=$(echo $image | cut -d '/' -f2-)
 
-  echo "[INFO] Copying "$image
+  echo "[INFO] Copying: "$image
 
   n=0
   max=5
@@ -23,9 +24,15 @@ do
   do
     skopeo copy --dest-tls-verify=false --format=v2s2 docker://$image docker://${DOCKER_REGISTRY}/$name && break
     n=$[$n+1]
-    echo "[ERROR] Failed to copy "$image
-    echo "[ERROR] Try "$n" of "$max" in 5 seconds"
-    sleep 5s
+    echo "[ERROR] Failed to copy: "$image
+    echo "[ERROR] Try "$n" of "$max" in 3 seconds"
+    sleep 3s
   done
+
+  if [[ $n == $[$max] ]]; then
+    echo "[ERROR] Fatal error. Failed to copy: "$image
+    exit 1
+  fi
+
 done
 echo "[INFO] Finished"
